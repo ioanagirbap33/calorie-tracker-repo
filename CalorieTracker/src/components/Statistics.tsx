0c833122772d6b8103696378301a4347df75b56d
ioana.girba <>
2024-03-31 21:54:41 +0300

    Change chart opacity

--- CalorieTracker/src/components/Statistics.tsx
+++ CalorieTracker/src/components/Statistics.tsx
@@ -29,9 +29,9 @@ export const Statistics = () => {
   };
   const chartConfig = {
     backgroundGradientFrom: '#1E2923',
-    backgroundGradientFromOpacity: 0,
+    backgroundGradientFromOpacity: 0.5,
     backgroundGradientTo: '#08130D',
-    backgroundGradientToOpacity: 0.5,
+    backgroundGradientToOpacity: 0.75,
     color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
     strokeWidth: 2,
     barPercentage: 0.5,
