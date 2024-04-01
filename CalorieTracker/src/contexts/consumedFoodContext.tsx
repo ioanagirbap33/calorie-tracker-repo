4d5df74391cecc53c84ce167567b80aaab767a67
ioana.girba <>
2024-04-01 10:43:01 +0300

    Add consumed food to local storage

--- CalorieTracker/src/contexts/consumedFoodContext.tsx
+++ CalorieTracker/src/contexts/consumedFoodContext.tsx
@@ -1,5 +1,6 @@
-import {createContext, useContext, useState, Dispatch} from 'react';
+import {createContext, useContext, useState, Dispatch, useEffect} from 'react';
 import {FoodDetailsType} from '../components/FoodDetailsCard';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export type ConsumedFoodContextProp = {
   consumedFood: FoodDetailsType[];
@@ -25,6 +26,21 @@ export const ConsumedFoodProvider: React.FC<{children: React.ReactNode}> = ({
   children,
 }) => {
   const [consumedFood, setConsumedFood] = useState<FoodDetailsType[]>([]);
+
+  useEffect(() => {
+    const fetchDataFromStorage = async () => {
+      try {
+        const storedData = await AsyncStorage.getItem('consumedFood');
+        if (storedData !== null) {
+          setConsumedFood(JSON.parse(storedData));
+        }
+      } catch (error) {
+        console.error('Error retrieving data from local storage:', error);
+      }
+    };
+    fetchDataFromStorage();
+  }, []);
+
   return (
     <ConsumedFoodContext.Provider value={{consumedFood, setConsumedFood}}>
       {children}
